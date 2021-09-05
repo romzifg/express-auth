1a4a0f03ede2a01eb745d1d114824c6e7b6c424b
Romzi Farhan <romzifg@gmail.com>
2021-09-05 17:42:56 +0700

    add validation

--- model/User.js
+++ model/User.js
@@ -4,25 +4,21 @@ const userSchema = new mongoose.Schema({
     full_name: {
         type: String,
         required: true,
-        min: 3,
         max: 255
     },
     username: {
         type: String,
         required: true,
-        min: 6,
         max: 255
     },
     email: {
         type: String,
         required: true,
-        min: 6,
         max: 255
     },
     password: {
         type: String,
         require: true,
-        min: 8,
         max: 1024
     },
     date: {
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+// Validation
+const Joi = require('@hapi/joi');
+const schema = Joi.object({
+    full_name: Joi.string().min(3).required(),
+    username: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
+
 router.post('/register', (req, res) => {
+
+    // Validate Data before save
+    const { error } = schema.validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    // save data if not error validation
     const user = new User({
         full_name: req.body.full_name,
         username: req.body.username,
